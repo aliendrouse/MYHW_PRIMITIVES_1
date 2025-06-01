57ce0fa443f8e31c607897e580fe905773f831d2
MarinaM <jokerbunny@gmail.com>
2025-06-01 12:25:30 +0300

    task completed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15000;
+        int rublesForMile = 20;
+        int bonus = amount / rublesForMile;
+
+        System.out.println(bonus + " бонусов");
+    }
+}
